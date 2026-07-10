13e1ec7ab4ddba109a0d61bf457ba0fd4bd827f7
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-07-10 17:48:25 +0200

    Esempi operazioni

---
+++ giorno1/operazioni.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 4; 
+    // int b = 5;
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%.2f\n", (double)a / b);
+    printf("%d\n", a * b);
+    printf("%d\n", a % b);
+    return 0;
+}
---
+++ giorno1/operazioni.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a = 30, b = 4;
+    
+    std::cout << "a + b = " << a + b << "\n";
+    std::cout << "a - b = " << a - b << "\n";
+    std::cout << "a * b = " << a * b << "\n";
+    std::cout << "a / b = " << (double)a / b << "\n";
+    std::cout << "a % b = " << a % b << "\n";
+
+    return 0;
+}
