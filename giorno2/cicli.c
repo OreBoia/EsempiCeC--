94543a3fcf5e52da29f77e9d4ad7c1317f414260
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-07-17 16:56:50 +0200

    Esempio cicli

---
+++ giorno2/cicli.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    //WHILE
+    // int n = 5;
+    // while(n > 0)
+    // {
+    //     printf("%d\n", n);
+    //     n--; //diminuisce di 1 il valore in n
+    // }
+
+    // int scelta = 0;
+
+    // while (scelta < 1 || scelta > 5)
+    // {
+    //     printf("\n1)somma\n2)sottrazione\n3)moltiplicazione\n4)divisione\n5)esci\n");
+    //     scanf("\n%d", &scelta);
+
+    //     if(scelta == 5){
+    //         printf("uscita forzata");
+    //         break;
+    //     }
+
+    //     printf("\nhai scelto %d", scelta);
+    // }
+
+    //DO-WHILE
+    // int s = 1;
+    // do{
+    //     printf("inserisci un numero positivo: \n");
+    //     scanf("%d", &s);
+    // }while(s <= 0);
+
+    //FOR
+    // for(int i = 0; i < 10; i++) //i++ -> i = i + 1 -> i += 1
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    return 0;
+}
